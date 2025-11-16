7475f0ba3cb2b03e2a0e2952cb6a94fe254e0fe0
michaelmazilu <m.mazilu@icloud.com>
2025-11-15 21:36:56 -0500

    remove devtools

--- devtools/app/api/reload/status/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { getLastRestartInfo } from "../../server";
-
-export const runtime = "nodejs";
-
-export async function GET() {
-  const info = getLastRestartInfo();
-  return NextResponse.json(info);
-}
